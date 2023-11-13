89c52c50b28c81082c5f5d653967dafa9c10a27b
md arif <arifahd92@gmail.com>
2023-11-14 00:51:20 +0530

    get message api

--- backend/controllers/chat.js
+++ backend/controllers/chat.js
@@ -1,4 +1,5 @@
 const Chat = require("../modals/chat");
+const User = require("../modals/user");
 
 //m-post => /chat
 const saveChat = async (req, res, next) => {
@@ -15,4 +16,24 @@ const saveChat = async (req, res, next) => {
     res.status(500).send({ mesage: error.message });
   }
 };
-module.exports = { saveChat };
+// m-get =>/chat
+const getChat = async (req, res) => {
+  try {
+    // Fetch all chat messages along with user details
+    const messages = await Chat.findAll({
+      attributes: ["message", "createdAt"],
+      include: [
+        {
+          model: User,
+          attributes: ["name", "email"],
+        },
+      ],
+    });
+
+    res.json(messages);
+  } catch (error) {
+    console.error("Error:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+module.exports = { saveChat, getChat };
--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -1,5 +1,6 @@
-const { saveChat } = require("../controllers/chat");
+const { saveChat, getChat } = require("../controllers/chat");
 const { authorize } = require("../middleware/authorize");
 const router = require("express").Router();
 router.post("/chat", authorize, saveChat);
+router.get("/chat", authorize, getChat);
 module.exports = router;
